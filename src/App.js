fe0c1776819f1bb80e48d7fa87fdcd4775670a4f
eriksun0310 <erikawang1233@gmail.com>
2024-09-29 11:07:16 +0800

    add mui switch ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
-// import Box from "./Box";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-// import Diamond from "./Diamond";
 import GLTFModel from "./GLTFModel";
 import MarioStart from "./MarioStart";
 import DisplayCase from "./DisplayCase";
 import Lamp from "./Lamp";
-import Switch from "./Switch";
+
 import MuiSwitch from "./MUISwitch";
 
 function App() {
-  const [ambientLightIntensity, setAmbientLightIntensity] = useState(10);
-  const toggleLight = () => {
-    setAmbientLightIntensity((prev) => (prev > 0 ? 0 : 10)); // 切换环境光强度
-  };
-
   // 定义开关状态
   const [isOff, setIsOff] = useState(false);
 
-  console.log("isOff", isOff);
   return (
     <>
       <div
